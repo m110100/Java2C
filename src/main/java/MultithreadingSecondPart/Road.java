058b2a439209ac820c64e8a51e837d9b89884ce9
m110100 <shindayoni@gmail.com>
2022-10-17 14:11:39 +0300

    Update Road.java

--- src/main/java/MultithreadingSecondPart/Road.java
+++ src/main/java/MultithreadingSecondPart/Road.java
@@ -12,7 +12,7 @@ public class Road extends Stage {
         try {
             System.out.printf("%s started the stage: %s%n", c.getName(), description);
 
-            Thread.sleep(length / c.getSpeed() * 1000);
+            Thread.sleep(length / c.getSpeed() * 1000L);
 
             System.out.printf("%s finished the stage: %s%n", c.getName(), description);
         }
